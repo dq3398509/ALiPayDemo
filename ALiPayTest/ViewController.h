48251ec7212053b8645bac61a84f1d79c9064ecf
董强 <531232071@qq.com>
2016-03-07 01:06:03 +0800

    Initial Commit

---
+++ ALiPayTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALiPayTest
+//
+//  Created by 董强 on 16/3/7.
+//  Copyright © 2016年 董强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ALiPayTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ALiPayTest
+//
+//  Created by 董强 on 16/3/7.
+//  Copyright © 2016年 董强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
